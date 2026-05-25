cfb188b194f55a3524424e7a6cc1fda254144a2a
ankit <rohitankit@387gmail.com>
2026-05-25 10:44:17 +0530

    Your changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,20 +1,8 @@
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
 import "./design/dashboad.css";
 
 function Dashboard() {
   const isLoggedIn = localStorage.getItem("isLoggedIn");
-=======
-import { useNavigate } from "react-router-dom";
-
-
-function Dashboard() {
-  const navigate = useNavigate();
-
-  const isLoggedIn = localStorage.getItem(
-    "isLoggedIn"
-  );
->>>>>>> 1b64b9f71a8519be5886ebe9679f618a896a6590
 
   const handleLogout = () => {
     localStorage.removeItem("isLoggedIn");
@@ -60,53 +48,21 @@ function Dashboard() {
 
       {/* MAIN */}
 
-<<<<<<< HEAD
       <main>
-        <h1 style={{ fontFamily: "revert-layer" }}>Welcome to</h1>
-        <b
-          className="dashboard-description"
-          style={{ fontFamily: "fangsong", fontSize: 60, width: "100%" }}
-        >
-          Ayurveda
-        </b>
-        <span
-          style={{
-            fontSize: 30,
-            color: "rgb(51, 78, 36)",
-            fontFamily:
-              "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
-          }}
-        >
+        <h1 className="welcome" style={{ fontFamily: "revert-layer" }}>
+          Welcome to
+        </h1>
+        <b className="dashboard-description">Ayurveda</b>
+        <br></br>
+        <span className="dashboard-des">
           Discover Your Ayurvedic Constitution
         </span>
-        <br></br>
-        <span
-          style={{
-            fontSize: 20,
-            color: "rgb(51, 78, 36)",
-            fontFamily: "cursive",
-          }}
-        >
+
+        <span className="dashboard-de">
           Analyze your biological energies (Doshas) through our refined physical
           and behavioral assessment, receive curated dietary and yoga practices,
           and explore critical medication safety alerts.
         </span>
-=======
-      <main
-        style={{
-          textAlign: "center",
-          marginTop: "100px"
-        }}
-      >
-
-        <h1>
-          Welcome to Tridosha AI
-            <button onClick={() => navigate("/assessment")}>
-        Dosha Test
-      </button>
-        </h1>
-
->>>>>>> 1b64b9f71a8519be5886ebe9679f618a896a6590
       </main>
     </section>
   );
